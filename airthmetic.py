16de49dcc29eff6919793f3aae8924e77d8f6ef0
amansrmuu <109527638+amansrmuu@users.noreply.github.com>
2022-07-25 17:38:35 +0530

    smlabook

---
+++ airthmetic.py
@@ -0,0 +1,7 @@
+a = 78
+b = 89
+c = 56
+
+print (a*b*c)
+print(a+b+c)
+print(a-b)+c
---
+++ simple.py
@@ -0,0 +1,36 @@
+p = 18000
+r = 20.0
+t = 10
+
+si = (p*r*t)/100
+print(si)
+
+a = 13
+b = 34
+c =56
+d =78
+e =90
+
+avg = (a+b+c+d+e)/5
+print(avg)
+
+a = 23
+b = 56
+
+sum = a+b
+print(sum)
+z = sum
+f = z*5
+print(f)
+
+a = 34
+b = 56
+if(a>b):
+    print("value is 5")
+print("value is less")    
+
+a = 45
+b = 36
+if(a>b):
+    print("a is greater" , a)
+print("b is smaller", b)
