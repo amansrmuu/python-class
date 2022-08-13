54d28455134167c7925ce1d427c521abd24a90f6
amansrmuu <109527638+amansrmuu@users.noreply.github.com>
2022-08-13 09:20:55 +0530

    kk

---
+++ Data structure/cv.py
@@ -0,0 +1,3 @@
+
+ = ['batman','ddlj','krish','3 idiot','rrr','kgf','doctor srange','avenblockbusters = movies.copy()
+print('duplicated list = ', blockbusters)
---
+++ Data structure/turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+s = getscreen()
+t= Turtle()
+t.fd(100)
+t.lt(72)
+t.fd(100)
+t.lt(72)
+t.fd(100)
+t.lt(72)
+t.fd(100)
+t.lt(72)
+t.fd(100)
+t.lt(72)
+
+mainloop()
